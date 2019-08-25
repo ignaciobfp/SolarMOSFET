3032410a69a1ee49a1da65cc9da0c5eb4033816a
Ignacio Barrio <ignaciobfp@gmail.com>
2019-08-25 18:59:40 +0200

    Simplified logic, new vref

--- solarMosfet2S.ino
+++ solarMosfet2S.ino
@@ -4,17 +4,14 @@ const int ledPin = 13;
 const int mosfetGatePin = 4;
 
 //Auxiliary values
-const float refVoltage = 4.97;
+const float refVoltage = 4.86;
 const float vDividerFactor = 2.0;
-const float batMaxVoltage = 8.6; //2s LiPo battery, i.e. 2*4.25V. Use sightly lower values to preserve battery heatlh.
-const float batThresholdVoltage = 8.3; //If battery falls below this voltage and some time passed since last charge, we start charging again
+const float batMaxVoltage = 8.4; //2s LiPo battery, i.e. 2*4.2V. Use sightly lower values to preserve battery heatlh.
 const float batMinVoltage = 7.9; //If battery falls below this voltage, we always turn on charge
-const float maxCycles = 60;
-const int secondsBetweenChecks = 1;
+const int secondsBetweenChecks = 0.25;
 
 //Global definitions
 float vBat = 0.0;
-unsigned int cyclesSinceLastCharge = 0;
 bool isCharging = false;
 
 
@@ -26,7 +23,6 @@ inline void startSolarCharge() {
   if (!isCharging) {
     digitalWrite(mosfetGatePin, HIGH);
     digitalWrite(ledPin, HIGH);
-    cyclesSinceLastCharge = 0;
     isCharging = true;
   }
 }
@@ -35,7 +31,6 @@ inline void stopSolarCharge() {
   //We do not check if isCharging is true here, because we want to stop always and as soon as possible
   digitalWrite(mosfetGatePin, LOW);
   digitalWrite(ledPin, LOW);
-  cyclesSinceLastCharge = 0;
   isCharging = false;
 }
 
@@ -43,27 +38,19 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(mosfetGatePin, OUTPUT);
   stopSolarCharge();
-  Serial.begin(9600);
-  Serial.println("Init OK");
+  //Serial.begin(9600);
+  //Serial.println("Init OK");
 }
 
 void loop() {
   checkVBat();
-  Serial.print("Batt: ");
-  Serial.print(vBat, 2);
-  Serial.println();
+  //Serial.print("Batt: ");
+  //Serial.print(vBat, 2);
+  //Serial.println();
   if (vBat > batMaxVoltage) {
     stopSolarCharge();
-  } else if (vBat < batThresholdVoltage && cyclesSinceLastCharge > maxCycles) {
-    startSolarCharge();
   } else if (vBat < batMinVoltage) {
     startSolarCharge();
   }
-  //Note there is a few edge cases that fall outside the prior conditional block.
-  //That are precisely the cases in which we want to stay in the same state, so we do nothing
-
-  if (!isCharging)
-    ++cyclesSinceLastCharge;
-
   delay(1000 * secondsBetweenChecks);
 }
